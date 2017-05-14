1f1b7af3fcb8edb3e984789a63e2baba1b7e4687
aritrac <aritrachatterjee2007@gmail.com>
2017-05-14 19:28:18 +0530

    Commit 1

---
+++ Declare JavaScript Objects as Variables.js
@@ -0,0 +1,23 @@
+/*
+Comments:
+Before we dive into Object Oriented Programming, let's revisit JavaScript objects.
+Give your motorBike object a wheels, engines and seats attribute and set them to numbers.
+
+Tests:
+motorBike should have a engines attribute set to a number.
+motorBike should have a wheels attribute set to a number.
+motorBike should have a seats attribute set to a number.
+*/
+var car = {
+  "wheels":4,
+  "engines":1,
+  "seats":5
+};
+
+var motorBike = {
+
+  // Only change code below this line.
+  "engines":1,
+  "wheels":2,
+  "seats":2
+};
